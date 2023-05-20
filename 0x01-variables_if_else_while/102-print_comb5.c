404fcd5bc2408bfcee3c785ef0eebd07700ee6b8
AdoleyineP <adoleyineprinceakolgo@gmail.com>
2023-05-20 12:35:34 +0000

    combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,14 +13,14 @@ int main(void)
 		{
 			for (k = 48; k < 58; k++)
 			{
-				for (l = 48; l < 58; l++)
+				for (l = 49; l < 58; l++)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(' ');
 					putchar(k);
 					putchar(l);
-					if (i != 9 && j !=8 && k != 9 && l != 9)
+					if ((i != 9 && j != 9) && (k != 9 && l != 9))
 					{
 						putchar(',');
 						putchar(' ');
