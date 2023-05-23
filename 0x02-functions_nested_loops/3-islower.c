92c944b52316a45e45ec5bdeedafe42840242342
AdoleyineP <adoleyineprinceaklgo@gmail.com>
2023-05-23 21:45:28 +0000

    display 1 if lower 0 otherwise

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,11 @@
 /**
  * _islower - Functions check if alphabet is lower case
  * @c: input to be received by the function
- * Return: 1 if input is lowercase and 0 for something else 
+ * Return: 1 if input is lowercase and 0 for something else
  */
 int _islower(int c)
 {
-	if ( c > 96 && c < 123)
-	{
+	if (c > 96 && c < 123)
 		return (1);
-	} else
-	{
-		return (0);
-	}
+	return (0);
 }
