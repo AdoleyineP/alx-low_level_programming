c9a1a9dcd1c7f69992118a7cd88ce6cf5eceb8ac
AdoleyineP <adoleyineprinceaklgo@gmail.com>
2023-05-23 20:53:26 +0000

    display alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Function to display alphabets
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
