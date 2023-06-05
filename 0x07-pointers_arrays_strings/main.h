033c4226b87ae70631927e5f055e7fe2a02e7089
AdoleyineP <adoleyineprinceaklgo@gmail.com>
2023-06-05 21:10:43 +0000

    all

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,5 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char a);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
+char *_strstr(char *haystack, char *needle);
 #endif
