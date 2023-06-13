6383472e751cb7587630a4a45997a7ad694dc1b6
adoleyineprinceakolgo@gmail.com <adoleyineprinceakolgo@gmail.com>
2023-06-13 23:43:16 +0000

    all

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * argstostr - function
+ * @ac: parameter
+ * @av: parameter
+ * Return: string pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int count = 0, j, i, k = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			count++;
+	}
+
+	str = malloc(sizeof(str) * count);
+	if (str == NULL)
+	{
+		free(str);
+		putchar('\0');
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			if (av[i][j + 1] == '\0')
+			{
+				str[k] = av[i][j];
+				str[k + 1] = '\n';
+				k += 2;
+			}
+			else
+			{
+				str[k] = av[i][j];
+				k++;
+			}
+
+		}
+	}
+
+	return (str);
+}
