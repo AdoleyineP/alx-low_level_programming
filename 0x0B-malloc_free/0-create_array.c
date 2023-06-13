d16c29ae427cc95559b0b5bc5bb3ccb7ac3a99e2
adoleyineprinceakolgo@gmail.com <adoleyineprinceakolgo@gmail.com>
2023-06-13 22:35:36 +0000

    create array with dynamic size

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - function that creates array with size provided by the user
+ * @size: size of the array
+ * @c: initializer of array
+ * Return: Null if size is 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	int i;
+
+	array = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+
+		array[i] = c;
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	return (array);
+}
