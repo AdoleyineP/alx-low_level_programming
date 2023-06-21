1d565f6a87b7417de04ecdd64167d0b24bb3ad06
AdoleyineP <adoleyineprinceakolgo@gmail.com>
2023-06-21 11:04:15 +0000

    all

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of code
+ * @argc: the number of arguments
+ * @argv: pointer to an array of strings
+ * Return: 0 if success
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-    printf("%d\n", argc - 1);
-    return 0;
+	printf("%d\n", argc - 1);
+	return (0);
 }
 
