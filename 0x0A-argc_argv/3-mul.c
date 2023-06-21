c343a58ebd01a9aa5392fa4001bcb271e44d4601
AdoleyineP <adoleyineprinceakolgo@gmail.com>
2023-06-21 11:16:27 +0000

    all

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3) {
+	int mul;
+
+	if (argc != 3)
 		printf("Error\n");
-		return (1);
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
 	}
-
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
-
-	printf("%d\n", result);
 	return (0);
 }
-
